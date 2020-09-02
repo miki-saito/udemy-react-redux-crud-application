aabd905a8de44bf3517999f188e76e5375377ca3
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-02 17:14:13 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+
+const  Cat = () => {
+  return <div>Meow!</div>
+}
+
+
 
 // function App() {
 //   return <div>Hello, world!</div>;
 // }
 
-class App extends Component {
-  render(){
-    return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}}/>
-    </React.Fragment>
-  )
-  }
-}
+// class App extends Component {
+//   render(){
+//     return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}}/>
+//     </React.Fragment>
+//   )
+//   }
+// }
 
 // class App extends Component {
 //   render(){
