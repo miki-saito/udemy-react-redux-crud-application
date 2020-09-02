2a139ab392c6ccabc32e670a6911ea39e917844b
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-02 18:33:57 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,35 @@
 import React from 'react';
 
-
 const App = () => {
+  const profiles = [
+    { name: "Taro", age:10 },
+    { name: "Hanako", age:5 },
+    { name: "Noname" }
+  ]
   return (
   <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
+    {
+      profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
   </div>
   )
 }
+// const App = () => {
+//   return (
+//   <div>
+//     <User name={"Taro"} age={10}/>
+//     <User name={"Hanako"} age={5}/>
+//   </div>
+//   )
+// }
 
-const  Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
 }
 
-
-
-// function App() {
-//   return <div>Hello, world!</div>;
-// }
-
-// class App extends Component {
-//   render(){
-//     return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {console.log("I am clicked")}}/>
-//     </React.Fragment>
-//   )
-//   }
-// }
-
-// class App extends Component {
-//   render(){
-//     const greeting = "Hi! Tom";
-//     const dom = <h1 className="foo">{greeting}</h1>;
-//     return  dom;
-//   }
-// }
-
-// class App extends Component {
-//   render(){
-//     return  <h1>"Hello, world!?!"</h1>;
-//   }
-// }
-
-
-
-// class App extends Component {
-//   render(){
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, world!!"
-//     );
-//   }
-// }
+User.defaultProps = {
+  age: 1
+}
 export default App;
