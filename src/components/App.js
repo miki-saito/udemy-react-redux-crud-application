deec08d9296ff7d0a19536337f4e48324dee6a9c
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-03 18:04:59 +0900

    implemnt component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
     const props = this.props
 
-      return(
+      return (
       <React.Fragment>
         <div>value: { props.value }</div>
         <button onClick={props.increment}>+1</button>
@@ -18,9 +18,11 @@ class App extends Component {
 }
 
 const mapStateToProps = state => ({ value: state.count.value })
-const mapDispatchToProps = dispatch => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement())
-})
+const mapDispatchToProps = ({ increment, decrement })
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
 
 export default connect (mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Provider } from 'react-redux'
 import './index.css';
 import reducer from './reducers'
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+// import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(reducer)
 
@@ -16,4 +17,11 @@ ReactDOM.render(
     </Provider>,
      document.getElementById('root')
 );
-registerServiceWorker();
+serviceWorker.register();
+// registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+// serviceWorker.unregister();
