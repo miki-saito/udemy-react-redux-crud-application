a948f5d0b98e5cabb92701bbf9e0b2390b52638d
miki-saito <65391589+miki-saito@users.noreply.github.com>
2020-09-03 13:55:21 +0900

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,14 @@
+import { INCREMENT, DECREMENT } from '../actions'
+
+const initialState = { value: 0 }
+
+export decault (state = initialState, action) => {
+    switch (action.tupe) {
+        case INCREMENT:
+            return { value: state.value + 1 }
+        case DECREMENT:
+            return { value: state.value - 1 }
+        default:
+            return state
+    }
+}
